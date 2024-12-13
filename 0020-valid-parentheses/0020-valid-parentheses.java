754be8d5cc149241e7aec21f525c171a9cba97ee
kongmin <amabile29@naver.com>
2024-12-14 00:45:19 +0900

    Time: 5 ms (9.54%), Space: 42.1 MB (15.07%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public static final Character[][] array = {
+        {'(', ')'}, {'[', ']'}, {'{', '}'}
+    };
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            Character chr = Character.valueOf(s.charAt(i));
+            
+            if (stack.empty()) {
+                stack.push(chr);
+                continue;
+            }
+
+            Character lastOne = stack.peek();
+            for (int j = 0; j < array.length; j++) {
+                if (array[j][1] == chr) {
+                    if (array[j][0] == lastOne) {
+                        stack.pop();
+                    } else {
+                        stack.push(chr);
+                    }   
+                } else if (array[j][0] == chr) {
+                    stack.push(chr);
+                }
+            }
+        }
+
+        return stack.empty();
+    }
+}
